94f36cb2c7ce9e85c4b59ab3c296bddf60abb8db
Jayprobot <106929692+Jayprobot@users.noreply.github.com>
2022-12-20 13:37:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,12 +4,13 @@ Myself <a href=https://t.me/{}>{}</a>,\n\nTrust me ! I can't even imagine how su
     HELP_TXT = """ʜᴇʏ {}
 Here is the help for my COMMANDS."""
     ABOUT_TXT = """✯ ᴍʏ ɴᴀᴍᴇ: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Syrus_143_hpy>亗 𝕊𝗔𝗡𝗗𝗬 |⚡|</a>
-✯ ʟɪʙʀᴀʀʏ: ɪᴅᴋ
-✯ ʟᴀɴɢᴜᴀɢᴇ: ɪᴅᴋ
-✯ ʀᴇǫ ɢʀᴏᴜᴘ: <a href=https://t.me/+vRdXzcif8qM1Yjc1>ᴅᴋ | ᴇɴᴛᴇʀᴛᴀɪɴᴍᴇɴᴛ</a>"
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: ɪᴅᴋ
-✯ ʙᴜɪʟᴅ sᴛᴀᴛᴜs: v1.0.1 [ ɪᴅᴋ ]"""
+✥ ᴍʏ ᴏᴡɴᴇʀ : <a href=https://t.me/PharmacistBoy1>♨️sᴘᴇᴄɪᴀʟɪsᴛ♨️™️</a>"  
+✥ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/Syrus_143_hpy>亗 𝕊𝗔𝗡𝗗𝗬 |⚡|</a>
+✥ ʟɪʙʀᴀʀʏ: ɪᴅᴋ
+✥ ʟᴀɴɢᴜᴀɢᴇ: ɪᴅᴋ
+✥ ʀᴇǫ ɢʀᴏᴜᴘ: <a href=https://t.me/+vRdXzcif8qM1Yjc1>ᴅᴋ | ᴇɴᴛᴇʀᴛᴀɪɴᴍᴇɴᴛ</a>"
+✥ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: ɪᴅᴋ
+✥ ʙᴜɪʟᴅ sᴛᴀᴛᴜs: v1.0.1 [ ɪᴅᴋ ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Lazy Princess is an open source project. 
 - Owner - <a href=https://t.me/PharmacistBoy1>♨️sᴘᴇᴄɪᴀʟɪsᴛ♨️™️</a>"  
