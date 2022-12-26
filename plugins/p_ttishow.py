5fe5c200fcdcc26722ed7fc570abaee446e469ed
Jayprobot <106929692+Jayprobot@users.noreply.github.com>
2022-12-26 11:49:11 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('🤥 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🔔 Updates', url='https://t.me/Filmy_Men')
+            InlineKeyboardButton('🔔 Updates', url='https://t.me/newyear2023group')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
