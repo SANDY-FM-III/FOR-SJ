ec28709b3cdd927edf84aab0b1b87270dd461e20
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2023-03-21 15:49:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -364,7 +364,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False 
                 )
-                await query.answer('Check PM, I have sent files in pm/n/अपना मेसेज चेक करीये मैने मूवी भेज दी ही', show_alert=True)
+                await query.answer('Check PM, I have sent files in pm\n\nअपना मेसेज चेक करीये मैने मूवी भेज दी ही', show_alert=True)
         except UserIsBlocked:
             await query.answer('Unblock the bot mahn !', show_alert=True)
         except PeerIdInvalid:
