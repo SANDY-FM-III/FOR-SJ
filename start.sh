b8b40206faa0e599999f7ad79c26dcdca4fe455f
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2023-06-01 12:00:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
-
 then
-
   echo "Cloning main Repository"
-
   git clone https://github.com/SANDY-FM-III/FOR-SJ /FOR-SJ
-
 else
-
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-
   git clone $UPSTREAM_REPO /FOR-SJ
-
 fi
-
 cd /FOR-SJ
-
 pip3 install -U -r requirements.txt
-
 echo "Starting Bot...."
-
 python3 bot.py
